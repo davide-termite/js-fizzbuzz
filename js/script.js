ba65711f4b231d66f02e0daca690b657ff8e5ff6
Davide Termite <davide.termite@gmail.com>
2020-05-27 17:29:20 +0200

    Dom manipulation

--- js/script.js
+++ js/script.js
@@ -1,19 +1,28 @@
+// Importo ul da html
+var list = document.getElementById ("fizzbuzz");
+
+// Creo variabile vuota Output
+var output;
+
 //  Creo numeri da 1 a 100
 for (var i = 1; i <= 100; i++) {
 
   // Multipli di 3 e 5
   // if (i%15 === 0)
   if (i%3 === 0 && i%5 === 0){
-    console.log(i + " FizzBuzz");
+    output = (i + " FizzBuzz");
   }
   // Multipli di 3
   else if (i%3 === 0) {
-    console.log(i + " Fizz");
+    output = (i + " Fizz");
   }
   // Multipli di 5
   else if (i%5 === 0) {
-    console.log(i + " Buzz");
+    output = (i + " Buzz");
   } else {
-    console.log(i);
+    output = i
   }
+
+  // Importo output in HTML
+  list.innerHTML += '<li>' + output + '</li>';
 }
